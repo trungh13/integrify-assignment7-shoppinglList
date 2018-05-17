11e3d2d0b7b573f8c777bf368830aedb34af83f9
trungh13 <newbie.trungh13@gmail.com>
2018-05-17 13:31:10 +0300

    add Edit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const shoppingList = [
+let shoppingList = [
   { bought: false, name: "Tequila", quantity: 1 },
   { bought: false, name: "Triple Sec", quantity: 1 },
   { bought: true, name: "Glasses", quantity: 2 },
@@ -7,7 +7,7 @@ const shoppingList = [
   { bought: false, name: "Ice", quantity: 1 },
   { bought: true, name: "Shaker", quantity: 1 }
 ];
-
+const regexNum = /^[1-9]+[0-9]?$/;
 const btnSubmit = document.getElementById("btnSubmit");
 btnSubmit.addEventListener("click", createItem);
 
@@ -43,9 +43,19 @@ const renderItem = (item, parentNode) => {
   let itemName = document.createElement("div");
   itemName.className = "shopping-item-name";
   itemName.innerHTML = item.name;
+  let editName = document.createElement("input");
+  editName.className = "edit-item-name hide";
+  editName.value = item.name;
   let itemQuantity = document.createElement("div");
   itemQuantity.className = "shopping-item-quantity";
   itemQuantity.innerHTML = item.quantity;
+  let editQuantity = document.createElement("input");
+  editQuantity.className = "edit-item-quantity hide";
+  editQuantity.value = item.quantity;
+  let editButton = document.createElement("button");
+  editButton.className = "btnEdit button";
+  editButton.innerHTML = "Edit";
+  editButton.addEventListener("click", editItem);
   let deleteButton = document.createElement("button");
   deleteButton.className = "btnDelete button";
   deleteButton.innerHTML = "Delete";
@@ -53,7 +63,10 @@ const renderItem = (item, parentNode) => {
 
   itemNode.appendChild(itemCheckbox);
   itemNode.appendChild(itemName);
+  itemNode.appendChild(editName);
   itemNode.appendChild(itemQuantity);
+  itemNode.appendChild(editQuantity);
+  itemNode.appendChild(editButton);
   itemNode.appendChild(deleteButton);
 
   document.getElementById(parentNode).appendChild(itemNode);
@@ -70,25 +83,37 @@ function createItem() {
   event.preventDefault();
   const nameInput = document.getElementById("new-item-name");
   const quantityInput = document.getElementById("new-item-quantity");
-  let newName = nameInput.value;
-  let newQuantity = quantityInput.value;
+  const newName = nameInput.value;
+  const newQuantity = quantityInput.value;
+
+  const panel2 = document.getElementById("panel2");
+  const panel2a = panel2.querySelector("a");
+
   const duplicateName = shoppingList.findIndex(function(obj) {
     return obj.name === newName;
   });
   if (newName === "") {
     nameInput.placeholder = "Please enter name";
-    nameInput.className = "warning-input";
   }
-  if (newQuantity == "") {
-    quantityInput.placeholder = "Please enter quantity";
+  if (!regexNum.test(newQuantity)) {
+    quantityInput.value = "";
+    quantityInput.placeholder = "Re-enter quantity!";
     quantityInput.className = "warning-input";
   }
   if (duplicateName !== -1) {
     nameInput.value = "";
     nameInput.placeholder = "Duplicated name!";
-    nameInput.className = "warning-input";
   }
-  if (newName != "" && newQuantity != "" && duplicateName == -1) {
+  if (nameInput.placeholder != null) nameInput.className = "warning-input";
+  if (quantityInput.placeholder != null)
+    quantityInput.className = "warning-input";
+
+  if (
+    newName != "" &&
+    newQuantity != "" &&
+    duplicateName == -1 &&
+    newQuantity > 0
+  ) {
     const newItem = {};
     newItem.bought = false;
     newItem.name = newName;
@@ -98,8 +123,9 @@ function createItem() {
     document.getElementById("new-item-quantity").value = "";
     shoppingList.push(newItem);
     renderItem(newItem, "shopping-items-list");
-  }
 
+    panel2a.click();
+  }
   console.log(shoppingList);
 }
 
@@ -107,11 +133,51 @@ function createItem() {
 function deleteItem() {
   const item = event.target;
   const itemName = item.parentNode.id;
+  const itemNode = item.parentNode;
   const index = shoppingList.findIndex(function(obj) {
     return obj.name === itemName;
   });
-  const deleteItem = item.parentNode;
-  deleteItem.remove();
+  itemNode.remove();
   shoppingList.splice(index, 1);
   console.log(shoppingList);
+}
+
+function editItem() {
+  const item = event.target;
+  let itemName = item.parentNode.id;
+  const itemNode = item.parentNode;
+  const index = shoppingList.findIndex(function(obj) {
+    return obj.name === itemName;
+  });
+  let buttonName = itemNode.querySelector(".btnEdit");
+  const divName = itemNode.querySelector(".shopping-item-name");
+  const editName = itemNode.querySelector(".edit-item-name");
+  const divQuantity = itemNode.querySelector(".shopping-item-quantity");
+  const editQuantity = itemNode.querySelector(".edit-item-quantity");
+
+  if (buttonName.innerHTML === "Edit") {
+    buttonName.innerHTML = "Save";
+    divName.classList.add("hide");
+    editName.classList.remove("hide");
+    divQuantity.classList.add("hide");
+    editQuantity.classList.remove("hide");
+  } else {
+    buttonName.innerHTML = "Edit";
+    editName.classList.add("hide");
+    divName.classList.remove("hide");
+    editQuantity.classList.add("hide");
+    divQuantity.classList.remove("hide");
+    itemNode.id=editName.value;
+    shoppingList[index].name = editName.value;
+    divName.innerHTML = editName.value;
+    if (regexNum.test(editQuantity.value)) {
+      shoppingList[index].quantity = editQuantity.value;
+      divQuantity.innerHTML = editQuantity.value;
+      
+    }else{
+      editQuantity.value=shoppingList[index].quantity;      
+      
+    }
+  }
+  console.log(shoppingList[index]);
 }
